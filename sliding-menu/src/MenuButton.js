a48e95d41a2716d03d777572a646b71297b0a61c
Josh Wolsborn <wolsborn@pdx.edu>
2019-09-15 21:41:07 -0700

    Modified the menu button to prevent unnecessary renders

--- sliding-menu/src/MenuButton.js
+++ sliding-menu/src/MenuButton.js
@@ -2,6 +2,14 @@ import React, {Component} from "react";
 import "./MenuButton.css"
 
 class MenuButton extends Component{
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if(nextProps.handleMouseDown === this.props.handleMouseDown){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     render(){
         return (
             <button id="roundButton"
