7edd9899315e05a5fa168f8582789e776be28802
Josh Wolsborn <wolsborn@pdx.edu>
2019-09-17 23:36:37 -0700

    Added Contact, Home, and Stuff components

---
+++ react-spa/src/Contact.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+class Contact extends Component {
+    render(){
+        return (
+            <div>
+                <h2>GOT QUESTIONS?</h2>
+                <p>The easiest thing to do is check <a href="http://www.stackoverflow.com">StackOverflow</a>
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
---
+++ react-spa/src/Stuff.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+
+class Stuff extends Component {
+    render(){
+        return (
+            <div>
+                <h2>STUFF</h2>
+                <p>Viverra adipiscing at in tellus integer feugiat scelerisque varius morbi.</p>
+               <ol>
+                   <li>Nulla pulvinar diam</li>
+                   <li>Facilisis bibendum</li>
+                   <li>Vestibuluum vulputate</li>
+                   <li>Eget erat</li>
+                   <li>Id porttitor</li>
+               </ol>
+            </div>
+        )
+    }
+}
+
+export default Stuff;
